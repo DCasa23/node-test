efaed8a15560ed4305b60ecd8cbdc15ef5fa2956
DCasa23 <davidcasado1988@yahoo.es>
2023-01-16 16:28:24 +0100

    New Pokedex

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,27 +1,50 @@
 const express = require('express')
 const app = express()
 const port = 3005
+
+/*
+//Conexión a la Base de Datos
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', { userNewUrlParser: true, useUnifiedTopology: true });
+
+*/
+//Conexión a base de datos
+const mongoose = require('mongoose');
+//Variables que tendremos siempre:
+//Lo correcto será declararlas EN VARIABLES DE ENTORNO
+//para que nadie vea directamente nuestras credenciales
+const user = 'cursonode';
+const password = 'R9XhxeN7OITL7vDj';
+const dbname = 'database_pokemon';
+const uri = `mongodb+srv://${user}:${password}@cluster0.q4mbecl.mongodb.net/${dbname}?retryWrites=true&w=majority`; //URL de conexión, que completaremos luego
+mongoose.connect(uri,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
+
+
 //Motor de Plantillas
-app.set('views',__dirname + '/views');
-app.set('view engine','ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 //Middleware
 app.use(express.static(__dirname + '/public'));
 
 //Llamadas a las rutas:
-app.use('/',require('./router/rutas.js'));
-app.use('/pokemon',require('./router/pokemon.js'));
-app.use((req,res) => {
-    
-   res.status(404).render("404",
+app.use('/', require('./router/rutas.js'));
+app.use('/pokemon', require('./router/pokemon.js'));
+app.use((req, res) => {
+
+  res.status(404).render("404",
     {
-        cabecera:"Error 404",
-        tituloError:"ERROR DEL 404",
-        descripcion:"Page Not Found, Chiquillo. Ares dejame descansar"
-   })
-   })
-   
-   
+      cabecera: "Error 404",
+      tituloError: "ERROR DEL 404",
+      descripcion: "Page Not Found, Chiquillo. Ares dejame descansar"
+    })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
